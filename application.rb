41466a7f7c970c6fd6a9d75b693a51fea78f01cc
vsparrow <khan@vsparrow.com>
2018-03-11 09:52:35 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World\nHello, my name is Slim Shady"
     resp.finish
   end
 
 end
-
